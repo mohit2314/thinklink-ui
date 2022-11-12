dbeb98fb807c4d53146fbf2de545e7185d2f3bb2
mohit2314 <mohitpatel2314@gmail.com>
2022-11-12 17:02:31 +0530

    left sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <Navbar />
-    <!-- <Left /> -->
+    <LeftSidebar />
   </div>
 </template>
 <script>
 import Navbar from "@/components/Navbar.vue";
-// import Left from "@/components/LeftSidebar.vue"
+import LeftSidebar from "@/components/LeftSidebar.vue";
 export default {
   components: {
     Navbar,
-    // Left
+    LeftSidebar,
   },
 };
 </script>
---
+++ src/components/LeftSidebar.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="dashboard__sidebar">
+    <div
+      v-for="(item, i) in sidebarMenuList"
+      :class="['sidebar__list-item', { '--active': activeItem == i }]"
+      :key="i"
+      @click="activeItem = i"
+    >
+      <span><b-icon :icon="item.icon"></b-icon></span>
+      <span>{{ item.label }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeItem: 0,
+      sidebarMenuList: [
+        {
+          label: "Dashboard",
+          icon: "command",
+          // grid1x2
+          // tv
+        },
+        {
+          label: "Inventory",
+          icon: "layers",
+          // hdd-stack
+          // intersect
+          // stack
+        },
+        {
+          label: "Orders",
+          icon: "bag-check",
+        },
+        {
+          label: "Customers",
+          icon: "people",
+        },
+        {
+          label: "Reports",
+          icon: "file-text",
+        },
+        {
+          label: "Settings",
+          icon: "gear",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.dashboard__sidebar {
+  width: 250px;
+  height: 100vh;
+  border-right: 1px solid #ecf1f4;
+  .sidebar__list-item {
+    cursor: pointer;
+    padding: 12px;
+    display: flex;
+    gap: 16px;
+    font-weight: 600;
+
+    &.--active {
+      color: #811434;
+      background-color: #f7f1f3;
+      box-shadow: 2px 0px 0px #811434;
+    }
+  }
+}
+</style>
