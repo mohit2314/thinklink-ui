3dada59235c0034cdc78d8922874f3819c783dda
mohit2314 <mohitpatel2314@gmail.com>
2022-11-12 13:49:07 +0530

    installed bootstrap as mentioned

---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <b-navbar
+      toggleable="lg"
+      class="navbar"
+      type="dark"
+      style="background-color: #811434"
+    >
+      <b-navbar-brand href="#">
+        <img src="@/assets/brand-title.png" alt="" />
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <!-- <b-nav-item>
+        <b-icon icon="bell" variant="light"></b-icon>
+      </b-nav-item> -->
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <!-- <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown> -->
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+              <img src="@/assets/avtar.png" alt="" />
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+<style lang="scss">
+.navbar {
+  max-height: 56px;
+}
+</style>
